0151267475bc88ec20aefcaa85001b34fa7179eb
mohammad0x <mohammadtaghdir7@gmail.com>
2023-05-15 16:21:40 +0330

    register

--- api/serializers.py
+++ api/serializers.py
@@ -1,16 +1,16 @@
 from rest_framework import serializers
 from django.contrib.auth.models import User
-
+from .models import *
 # User Serializer
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = MyUser
         fields = ('id', 'username', 'email')
 
 # Register Serializer
 class RegisterSerializer(serializers.ModelSerializer):
     class Meta:
-        model = User
+        model = MyUser
         fields = ('id', 'username', 'email', 'password')
         extra_kwargs = {'password': {'write_only': True}}
 
--- api/views.py
+++ api/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-
-# Create your views here.
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
