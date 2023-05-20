8b1c43c9e21cfbb7f84c6712cdf1abea73645d4f
mohammad0x <mohammadtaghdir7@gmail.com>
2023-05-20 18:28:55 +0330

    register compelet

--- api/views.py
+++ api/views.py
@@ -23,16 +23,6 @@ class RegisterAPI(generics.GenericAPIView):
         "token": AuthToken.objects.create(user)[1]
         })
 
-# class LoginAPI(KnoxLoginView):
-#     permission_classes = (permissions.AllowAny,)
-#
-#     def post(self, request, format=None):
-#         serializer = AuthTokenSerializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         user = serializer.validated_data['user']
-#         login(request, user)
-#         return super(LoginAPI, self).post(request, format=None)
-
 
 class LoginAPI(KnoxLoginView):
     permission_classes = (permissions.AllowAny,)
